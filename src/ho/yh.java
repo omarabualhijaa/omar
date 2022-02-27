9e4c28a0346c3df42a625368de28ad710a27f7bb
omarabualhijaa <1933124@std.hu.edu.jo>
2022-02-28 02:49:15 +0300

    omarrr

---
+++ src/ho/yh.java
@@ -0,0 +1,12 @@
+package ho;
+
+public class yh {
+public static void main(String [] args){
+    System.out.println("oogggggggdfsdfoo");
+    System.out.println("ojciosanc");
+    System.out.println("osncvsdmc");
+    System.out.println("skanchsakdjcv");
+
+}
+
+}
